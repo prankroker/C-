863c7f83f9e5dcfe476cc4748e15e5efb8a5af59
prankroker <prankroker2@gmail.com>
2022-09-25 12:56:10 +0300

    dz

---
+++ dz/Lab4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min, max;
+            max = 0;
+            min = 0;
+            int[] arr = new int[5];
+            for(int i = 0; i < 5; i++)
+            {
+                arr[i] = i;
+                if(i == 0)
+                {
+                    max = i;
+                    min = i;
+                }
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                if(min > arr[i])
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+            arr[0] = max;
+            arr[4] = min; 
+            var str = string.Join(" ",arr);
+            Console.WriteLine(str);
+
+        }
+    }
+}
---
+++ dz/Laba3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Laba3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a,res;
+            string str;
+            Console.WriteLine("Введіть тризначне число:");
+            str = Console.ReadLine();
+            a = Double.Parse(str);
+            res = a % 2;
+            if (res == 0)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
