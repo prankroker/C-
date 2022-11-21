1fd92d430f0d2ca5fcc43e28b6c4daf866222c7d
prankroker <prankroker2@gmail.com>
2022-11-21 15:55:22 +0200

    laba7

---
+++ dz/Laba7/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Laba7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Створити ліст інтових змінних, який може вміщати тільки 1 і 0, заповнити його з клавіатури. 
+             * Порахувати кількість 1 і 0. 
+             * Скопіювати отриманий ліст в масив.
+             */
+            int a,one = 0,zero = 0;
+            string str;
+            List<int> list = new List<int>();
+            Console.WriteLine("Введіть змінні: ");
+            Console.WriteLine("Щоб закінчити введіть end");
+            while (true)
+            {
+                str = Console.ReadLine();
+                if (str.Equals("end"))
+                {
+                    break;
+                }
+                a = int.Parse(str); 
+                if(a == 1 || a == 0)
+                {
+                    list.Add(a);
+                }
+                else
+                {
+                    Console.WriteLine("Тільки 1 або 0");
+                    Console.WriteLine("Введіть змінні: ");
+                }
+            }
+
+            int[] arr = new int[list.Count];
+            list.CopyTo(arr);
+            
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] == 1)
+                {
+                    one++;
+                }
+                else
+                {
+                    zero++;
+                }
+            }
+            Console.WriteLine("one = " + one + "\n" + "zero = " + zero);
+        }
+    }
+}
